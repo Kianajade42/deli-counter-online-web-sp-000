cf4203ff262051e4a599fb6ebe0140c92cf51b0b
Kiana Long <kianajade42.kl@gmail.com>
2021-01-22 17:55:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+katz_deli=[]
+def line
+  if katz_deli == []
+  puts "There is nobody in line"
+else
+  puts "There are people in line"
+end
+ end
+ def take_a_number
+ end
+
+def now_serving
+end
