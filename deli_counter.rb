21681a38ef59ef63752fb1270cdf45e66c6c6185
Kiana Long <kianajade42.kl@gmail.com>
2021-01-22 18:11:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
-yeild_control=[]
+=[]
 def line(katz_deli)
-  if yeild_control == []
+  if == []
   puts "The line is empty"
 end
  end
@@ -8,4 +8,30 @@ end
  end
 
 def now_serving
+end
+
+def line(katz_deli)
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    current_line = "The line is currently:"
+    katz_deli.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"
+    end
+    puts current_line
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{deli.first}."
+    katz_deli.shift
+  end
 end
