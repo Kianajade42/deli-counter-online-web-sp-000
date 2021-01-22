049f833962ad73e04e3f188b8c88a77117883bf8
Kiana Long <kianajade42.kl@gmail.com>
2021-01-22 18:07:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
-katz_deli=[]
-def line
-  if katz_deli == []
+deli_counter=[]
+def line(katz_deli)
+  if deli_counter == []
   puts "There is nobody in line"
 else
   puts "There are people in line"
