c09510871ef5006fd5369e72c93995f45525effd
Kiana Long <kianajade42.kl@gmail.com>
2021-01-22 18:08:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,7 @@
 deli_counter=[]
 def line(katz_deli)
   if deli_counter == []
-  puts "There is nobody in line"
-else
-  puts "There are people in line"
+  puts "The line is empty"
 end
  end
  def take_a_number
